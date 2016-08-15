7121ec375f77f5b997e6a2cdda136b42e397e3b3
Alexander Hasselhuhn <alexander.hasselhuhn@tu-dortmund.de>
2016-08-15 18:01:51 +0200

    add example files

---
+++ examples/example.m
@@ -0,0 +1,17 @@
+(* Automatic *)
+Needs["SubKernels`RemoteKernels`"];
+Get["tunnel.m"];
+kernels = RemoteMachineTunnel["ahassel@ttp7of9.ttp.kit.edu", 2, "OperatingSystem" -> "Unix", "KernelPath" -> "/export/pc/bin/math"];
+kls = LaunchKernels[kernels];
+ParallelEvaluate[$MachineName];
+CloseKernels[kls];
+
+(* Manual *)
+Needs["SubKernels`RemoteKernels`"];
+$RemoteCommand = "\"" <> $UserBaseDirectory <> "/FrontEnd/tunnel_qsub.sh\" \"`1`\" /export/pc/bin/math \"`2`\"";
+Parallel`Settings`$MathLinkTimeout = 30;
+kernel = RemoteMachine["GridEngine", LinkHost -> "127.0.0.1"];
+ks = LaunchKernels[kernel];
+mn = ParallelEvaluate[$MachineName];
+Print[mn];
+Print[CloseKernels[ks]];
